d4ccf2d99d2f0bab72671a3d7ce2257682280faa
ioannis <olgagian@otenet.gr>
2020-07-13 02:17:58 +0300

    Initial Commit

---
+++ Letmeet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Letmeet
+//
+//  Created by ioannis giannakidis on 13/7/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
